53435037cd9aba810ce8aa7c7354d2173d22d197
Syafiq Kamarul Azman <syafiqkamarulazman@gmail.com>
2016-08-11 11:47:32 +0400

    Added new pages and title block for each page

--- app/jobs.py
+++ app/jobs.py
@@ -4,9 +4,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import BiomSearchJob
 
-context = {'flash': None}
+# general context for all pages
+context = {"flash": None}
 
-
+# TODO: Refactor later to use class based views for cleaner code
 @login_required
 def details(request, job_id):
     msg_storage = messages.get_messages(request)
@@ -21,10 +22,10 @@ def details(request, job_id):
                 file_name=job.biom_file.path.split('/')[-1]
             )
         context["file_path"] = file_path
-        context['job'] = job
-        context['criteria'] = ", ".join(map(str, job.criteria.all()))
-        context['flash'] = msg_storage
-        return render(request, 'app/job.html', context)
+        context["job"] = job
+        context["criteria"] = ", ".join(map(str, job.criteria.all()))
+        context["flash"] = msg_storage
+        return render(request, 'job/details.html', context)
     else:
         messages.add_message(
             request, messages.ERROR,
@@ -46,10 +47,155 @@ def remove(request, job_id):
             request, messages.SUCCESS,
             "Job {} successfully deleted".format(deleted_job_id)
         )
-        context['flash'] = msg_storage
+        context["flash"] = msg_storage
         return redirect('app:dashboard')
     else:
         messages.add_message(
             request, messages.ERROR, "Unauthorized access to Job " + job_id
+        )
+        return redirect('app:dashboard')
+
+
+@login_required
+def ranking(request, job_id):
+    msg_storage = messages.get_messages(request)
+    job = get_object_or_404(BiomSearchJob, id=job_id)
+
+    if job.user_id == request.user.id:
+        data_path = \
+            "{media_url}{user_id}/{job_id}/".format(
+                media_url=settings.MEDIA_URL,
+                user_id=request.user.id,
+                job_id=job_id,
+            )
+        context["data_path"] = data_path
+        context["job"] = job
+        context["flash"] = msg_storage
+        return render(request, 'job/ranking.html', context)
+    else:
+        messages.add_message(
+            request, messages.ERROR,
+            "Unauthorized access to Job {}".format(job.id)
+        )
+        return redirect('app:dashboard')
+
+
+@login_required
+def heatmap(request, job_id):
+    msg_storage = messages.get_messages(request)
+    job = get_object_or_404(BiomSearchJob, id=job_id)
+
+    if job.user_id == request.user.id:
+        data_path = \
+            "{media_url}{user_id}/{job_id}/".format(
+                media_url=settings.MEDIA_URL,
+                user_id=request.user.id,
+                job_id=job_id,
+                file_name=job.biom_file.path.split('/')[-1]
+            )
+        context["data_path"] = data_path
+        context["job"] = job
+        context["flash"] = msg_storage
+        return render(request, 'job/heatmap.html', context)
+    else:
+        messages.add_message(
+            request, messages.ERROR,
+            "Unauthorized access to Job {}".format(job.id)
+        )
+        return redirect('app:dashboard')
+
+
+@login_required
+def pcoa_reps(request, job_id):
+    msg_storage = messages.get_messages(request)
+    job = get_object_or_404(BiomSearchJob, id=job_id)
+
+    if job.user_id == request.user.id:
+        data_path = \
+            "{media_url}{user_id}/{job_id}/".format(
+                media_url=settings.MEDIA_URL,
+                user_id=request.user.id,
+                job_id=job_id,
+            )
+        context["data_path"] = data_path
+        context["job"] = job
+        context["flash"] = msg_storage
+        return render(request, 'job/pcoa_reps.html', context)
+    else:
+        messages.add_message(
+            request, messages.ERROR,
+            "Unauthorized access to Job {}".format(job.id)
+        )
+        return redirect('app:dashboard')
+
+
+@login_required
+def pcoa_similar(request, job_id):
+    msg_storage = messages.get_messages(request)
+    job = get_object_or_404(BiomSearchJob, id=job_id)
+
+    if job.user_id == request.user.id:
+        data_path = \
+            "{media_url}{user_id}/{job_id}/".format(
+                media_url=settings.MEDIA_URL,
+                user_id=request.user.id,
+                job_id=job_id,
+            )
+        context["data_path"] = data_path
+        context["job"] = job
+        context["flash"] = msg_storage
+        return render(request, 'job/pcoa_250.html', context)
+    else:
+        messages.add_message(
+            request, messages.ERROR,
+            "Unauthorized access to Job {}".format(job.id)
+        )
+        return redirect('app:dashboard')
+
+
+@login_required
+def dend_reps(request, job_id):
+    msg_storage = messages.get_messages(request)
+    job = get_object_or_404(BiomSearchJob, id=job_id)
+
+    if job.user_id == request.user.id:
+        data_path = \
+            "{media_url}{user_id}/{job_id}/".format(
+                media_url=settings.MEDIA_URL,
+                user_id=request.user.id,
+                job_id=job_id,
+            )
+        context["data_path"] = data_path
+        context["job"] = job
+        context["flash"] = msg_storage
+        return render(request, 'job/dend_reps.html', context)
+    else:
+        messages.add_message(
+            request, messages.ERROR,
+            "Unauthorized access to Job {}".format(job.id)
+        )
+        return redirect('app:dashboard')
+
+
+@login_required
+def dend_similar(request, job_id):
+    msg_storage = messages.get_messages(request)
+    job = get_object_or_404(BiomSearchJob, id=job_id)
+
+    if job.user_id == request.user.id:
+        data_path = \
+            "{media_url}{user_id}/{job_id}/".format(
+                media_url=settings.MEDIA_URL,
+                user_id=request.user.id,
+                job_id=job_id,
+            )
+        context["data_path"] = data_path
+        context["job"] = job
+        context["flash"] = msg_storage
+        return render(request, 'job/dend_250.html', context)
+    else:
+        messages.add_message(
+            request, messages.ERROR,
+            "Unauthorized access to Job {}".format(job.id)
         )
         return redirect('app:dashboard')
--- app/urls.py
+++ app/urls.py
@@ -20,6 +20,16 @@ urlpatterns = [
     url(r'^search/$', search.user_search, name='search'),
 
     url(r'^job/(?P<job_id>[0-9]+)$', jobs.details, name='job_details'),
+    url(r'^job/(?P<job_id>[0-9]+)/ranking$', jobs.ranking, name='job_ranking'),
+    url(r'^job/(?P<job_id>[0-9]+)/heatmap$', jobs.heatmap, name='job_heatmap'),
+    url(r'^job/(?P<job_id>[0-9]+)/pcoa-representatives$', jobs.pcoa_reps,
+        name='job_pcoa_reps'),
+    url(r'^job/(?P<job_id>[0-9]+)/pcoa-250$', jobs.pcoa_similar,
+        name='job_pcoa_similar'),
+    url(r'^job/(?P<job_id>[0-9]+)/dend-representatives$', jobs.dend_reps,
+        name='job_dend_reps'),
+    url(r'^job/(?P<job_id>[0-9]+)/dend-250$', jobs.dend_similar,
+        name='job_dend_similar'),
     url(r'^job/(?P<job_id>[0-9]+)/remove$', jobs.remove, name='job_remove'),
 ]
 
