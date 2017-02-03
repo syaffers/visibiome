3824d7e5a73c6584091774911e50d2de5b2455b4
Syafiq Kamarul Azman <syafiqkamarulazman@gmail.com>
2017-02-03 05:20:48 +0400

    Placeholder for registration page

--- app/models.py
+++ app/models.py
@@ -235,6 +235,9 @@ class UserForm(forms.ModelForm):
         }
         widgets = {
             "password": forms.PasswordInput,
+            "username": forms.TextInput(attrs={'placeholder': 'Username'}),
+            "email": forms.EmailInput(
+                attrs={'placeholder': 'example@email.com'}),
         }
 
     confirm = forms.CharField(
