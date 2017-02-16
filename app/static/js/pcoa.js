1ae96ecdc925ddbb1751bcbbdb10bde5967e2028
Syafiq Kamarul Azman <syafiqkamarulazman@gmail.com>
2017-02-16 15:40:21 +0400

    PCOA point labels bugfix

--- app/static/js/pcoa.js
+++ app/static/js/pcoa.js
@@ -18,31 +18,42 @@ function drawPcoa(dataPath, sampleIds) {
     $(this).addClass('active');
     $("#view-by-envo").removeClass('active');
     // reset when replotting pcoa
-    d3.select("svg")
-    .remove();
-    d3.select("svg")
-    .remove();
-    d3.select("svg")
-    .remove();
+    d3.select("svg").remove();
+    d3.select("svg").remove();
+    d3.select("svg").remove();
 
     var colorMap = d3.scale.ordinal()
-      .domain(
-        [' Freshwater', ' Plant', ' Soil', ' Animal/Human', ' Geothermal',
-         ' Marine', ' Anthropogenic', ' Biofilm', ' Unknown/User']
-       )
-      .range(
-        ['blue', 'Darkgreen', 'Gold', 'DarkViolet', 'SaddleBrown', 'Cyan',
-         'DarkOrange', 'SlateGray', 'Black']
-       );
+      .domain([
+        ' Freshwater',
+        ' Plant',
+        ' Soil',
+        ' Animal/Human',
+        ' Geothermal',
+        ' Marine',
+        ' Anthropogenic',
+        ' Biofilm',
+        ' Unknown/User'
+      ])
+      .range([
+        'blue',
+        'Darkgreen',
+        'Gold',
+        'DarkViolet',
+        'SaddleBrown',
+        'Cyan',
+        'DarkOrange',
+        'SlateGray',
+        'Black'
+      ]);
 
     drawAllPcoa('Ecosystem1', colorMap, margin, width, height, dataPath,
                 sampleIds, true);
   });
 
   // when user clicks the view by envo button
-  $("#view-by-envo").click(function (e) {
+  $("#view-by-envo").click(function (event) {
     // don't scroll back up
-    e.preventDefault();
+    event.preventDefault();
     // auto toggle
     $("#toggle-user-sample").addClass("active");
 
@@ -60,18 +71,18 @@ function drawPcoa(dataPath, sampleIds) {
   });
 
   // when user clicks toggle query label
-  $("#toggle-user-sample").click(function (e) {
+  $("#toggle-user-sample").click(function (event) {
      // hide/unhide label
     $(this).toggleClass("active");
-    $("svg .query-sample").toggle();
-    e.preventDefault();
+    $(".point-labels .query-sample").toggle();
+    event.preventDefault();
   });
 
   // when user clicks any of the download links
-  $("#plots .download-link").click(function (e) {
+  $("#plots .download-link").click(function (event) {
     var plot = $(this).data("plot");
     writeDownloadLink(plot);
-    e.preventDefault();
+    event.preventDefault();
   });
 
   $(document).ready(function() {
@@ -85,10 +96,10 @@ function drawPcoa(dataPath, sampleIds) {
  */
 function writeDownloadLink(plotId) {
   var html = d3.select("#" + plotId + " svg")
-      .attr("title", plotId.toUpperCase().split("-").join(" "))
-      .attr("version", 1.1)
-      .attr("xmlns", "http://www.w3.org/2000/svg")
-      .node().parentNode.innerHTML;
+        .attr("title", plotId.toUpperCase().split("-").join(" "))
+        .attr("version", 1.1)
+        .attr("xmlns", "http://www.w3.org/2000/svg")
+        .node().parentNode.innerHTML;
 
   window.open("data:image/svg+xml;base64,"+ btoa(html), '_blank');
 };
@@ -137,21 +148,21 @@ function drawPcXVsPcYByType(pcx, pcy, type, colorMap, width, height, margin,
 
   // add the graph canvas to the body of the webpage
   var svg = d3.select("#pcoa-pc" + pcx + "-pc" + pcy)
-  .append("svg")
-  .attr("font-family", "Helvetica, Arial, sans-serif")
-  .attr("width", width + 100 + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .append("svg")
+    .attr("font-family", "Helvetica, Arial, sans-serif")
+    .attr("width", width + 100 + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Add tooltip div
   var tooltip = d3.select("#pcoa-pc" + pcx + "-pc" + pcy + "-tooltip")
-  .append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 1e-6)
-  .style("font", 12)
-  .style("background", "#FFFF66")
-  .style("border", 0);
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 1e-6)
+    .style("font", 12)
+    .style("background", "#FFFF66")
+    .style("border", 0);
 
   d3.csv(dataPath, function(error, data) {
 
@@ -167,135 +178,146 @@ function drawPcXVsPcYByType(pcx, pcy, type, colorMap, width, height, margin,
 
     // draw x-axis
     svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-    .append("text")
-    .attr("class", "label")
-    .attr("x", width)
-    .attr("y", -6)
-    .style("text-anchor", "end")
-    .text("PC" + pcx);
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("PC" + pcx);
 
     // draw y-axis
     svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("class", "label")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("PC" + pcy);
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("PC" + pcy);
 
     // draw dotted line at x = 0
     svg.append("line")
-    .attr("class", "x line")
-    .attr("x1", xScale(0))
-    .attr("y1", 0)
-    .attr("x2", xScale(0))
-    .attr("y2", height)
-    .style('stroke', '#999999')
-    .style('stroke-width', 1)
-    .style('stroke-dasharray', "5, 5");
+      .attr("class", "x line")
+      .attr("x1", xScale(0))
+      .attr("y1", 0)
+      .attr("x2", xScale(0))
+      .attr("y2", height)
+      .style('stroke', '#999999')
+      .style('stroke-width', 1)
+      .style('stroke-dasharray', "5, 5");
 
     // draw dotted line at y = 0
     svg.append("line")
-    .attr("class", "y line")
-    .attr("x1", 0)
-    .attr("y1", yScale(0))
-    .attr("x2", width)
-    .attr("y2", yScale(0))
-    .style('stroke', '#999999')
-    .style('stroke-width', 1)
-    .style('stroke-dasharray', "5, 5");
+      .attr("class", "y line")
+      .attr("x1", 0)
+      .attr("y1", yScale(0))
+      .attr("x2", width)
+      .attr("y2", yScale(0))
+      .style('stroke', '#999999')
+      .style('stroke-width', 1)
+      .style('stroke-dasharray', "5, 5");
 
     // draw all points from data
-    svg.selectAll(".dot")
-    .data(data)
-    .enter().append("circle")
-    .attr("class", "dot")
-    .attr("r", 2.75)
-    .attr("cx", xMap)
-    .attr("cy", yMap)
-    .style("fill", function(d) {
-      // set user queried point to black
-      if (sampleIds.indexOf(d["Sample Name"]) >= 0)
-        return 'black';
-      return color(cValue(d));
-    })
-    .on("mouseover", function(d) {
-      tooltip.transition()
-      .duration(10)
-      .style("opacity", .9);
+    svg.append("g")
+      .attr("class", "points")
+      .selectAll("circle")
+      .data(data)
+      .enter()
+        .append("g")
+        .append ("circle")
+        .attr("class", "point")
+        .attr("r", 2.75)
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        .style("fill", function(d) {
+          // set user queried point to black
+          if (sampleIds.indexOf(d["Sample Name"]) >= 0)
+            return 'black';
+          return color(cValue(d));
+        })
+        .on("mouseover", function(d) {
+          tooltip.transition()
+          .duration(10)
+          .style("opacity", .9);
 
-      tooltip.html(
-        "Sample: " + d["Sample Name"] +
-        "<br>Ecosystem: " + d.Ecosystem1 + " " +
-        d.Ecosystem2.replace(' Unknown', '') + " " +
-        d.Ecosystem3.replace(' Unknown', '') +
-        "<br>Envo ID : " + d.OntologyID1 + ", " +
-        d.OntologyID2.replace(' Unknown', '') + " " +
-        d.OntologyID3.replace(' Unknown', '') +
-        "<br>Envo term: " + d.OntologyTerm1 + ", " +
-        d.OntologyTerm2.replace(' Unknown', '') + " " +
-        d.OntologyTerm3.replace(' Unknown', '') +
-        "<br>Study: " + d.Title + "<br>Study Source " + d["Study Source"]
-      )
-      .style("left", margin.left + xScale(d["PC" + pcx]) + "px")
-      .style("top", margin.top + yScale(d["PC" + pcy]) + "px");
-    })
-    .on("mouseout", function(d) {
-      tooltip.transition()
-      .duration(10)
-      .style("opacity", 0);
-    });
+          tooltip.html(
+            "Sample: " + d["Sample Name"] +
+            "<br>Ecosystem: " + d.Ecosystem1 + " " +
+            d.Ecosystem2.replace(' Unknown', '') + " " +
+            d.Ecosystem3.replace(' Unknown', '') +
+            "<br>Envo ID : " + d.OntologyID1 + ", " +
+            d.OntologyID2.replace(' Unknown', '') + " " +
+            d.OntologyID3.replace(' Unknown', '') +
+            "<br>Envo term: " + d.OntologyTerm1 + ", " +
+            d.OntologyTerm2.replace(' Unknown', '') + " " +
+            d.OntologyTerm3.replace(' Unknown', '') +
+            "<br>Study: " + d.Title + "<br>Study Source " + d["Study Source"]
+          )
+          .style("left", margin.left + xScale(d["PC" + pcx]) + "px")
+          .style("top", margin.top + yScale(d["PC" + pcy]) + "px");
+        })
+        .on("mouseout", function(d) {
+          tooltip.transition()
+          .duration(10)
+          .style("opacity", 0);
+        });
 
     // draw text label for the user queried sample point
-    svg.selectAll("text")
-    .data(data).enter()
-    .append("text")
-    .attr("class", "query-sample")
-    .attr("dx", xMap)
-    .attr("dy", yMap)
-    .attr("text-anchor", "start")
-    .attr("font-weight", "bold")
-    .attr('font-size', "10px")
-    .attr('fill', "red")
-    .text(function(d) {
-      if (sampleIds.indexOf(d["Sample Name"]) >= 0)
-        return "..................." + d["Sample Name"];
-      return "";
-    });
+    svg.append("g")
+      .attr("class", "point-labels")
+      .selectAll()
+      // only draw the text svg element for user submitted samples
+      .data(data.filter(function(sample) {
+        return sampleIds.indexOf(sample["Sample Name"]) >= 0
+      }))
+      .enter()
+        .append("text")
+        .attr("class", "query-sample")
+        .attr("x", xMap)
+        .attr("y", yMap)
+        .text(function(d) {
+          return "..................." + d["Sample Name"];
+        })
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .attr('font-size', "10px")
+        .attr('fill', "red")
 
   });
 
   // draw legend
   if (showLegend) {
-    var legend = svg.selectAll(".legend")
-    .data(color.domain())
-    .enter().append("g")
+    var legend = svg.append("g")
     .attr("class", "legend")
-    .attr("transform", function(d, i) {
-      var yCenter = ((height + margin.top) / 2.) - (legendHeight / 2.);
-      return "translate(10," + (yCenter + i * legendLabelHeight) + ")";
-    });
+    .selectAll(".legend")
+    .data(color.domain())
+    .enter()
+      .append("g")
+      .attr("class", "legend-item")
+      .attr("transform", function(d, i) {
+        var yCenter = ((height + margin.top) / 2.) - (legendHeight / 2.);
+        return "translate(10," + (yCenter + i * legendLabelHeight) + ")";
+      });
 
     // draw legend colored rectangles
     legend.append("rect")
-    .attr("x", width - 0)
-    .attr("width", legendLabelHeight / 2.)
-    .attr("height", legendLabelHeight / 2.)
-    .style("fill", color);
+      .attr("x", width - 0)
+      .attr("width", legendLabelHeight / 2.)
+      .attr("height", legendLabelHeight / 2.)
+      .style("fill", color);
 
     // draw legend text
     legend.append("text")
-    .attr("x", width +14)
-    .attr("y", 6)
-    .attr("dy", ".35em")
-    .style("text-anchor", "start")
-    .text(function(d) { return d;});
+      .attr("x", width +14)
+      .attr("y", 6)
+      .attr("dy", ".35em")
+      .style("text-anchor", "start")
+      .text(function(d) { return d;});
   }
 
 }
